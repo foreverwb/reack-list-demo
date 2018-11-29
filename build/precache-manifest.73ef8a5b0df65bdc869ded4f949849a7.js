b2b6dc4bc7777c6abcfa27bd5387f60645a2c26e
wangbin <wangbin01@btzh.cn>
2018-11-29 16:27:37 +0800

    first build

---
+++ build/precache-manifest.73ef8a5b0df65bdc869ded4f949849a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e401e406b7e3b8c3420b",
+    "url": "/static/js/main.e401e406.chunk.js"
+  },
+  {
+    "revision": "843b0fba9c92f4c1220b",
+    "url": "/static/js/1.843b0fba.chunk.js"
+  },
+  {
+    "revision": "e401e406b7e3b8c3420b",
+    "url": "/static/css/main.1e49f9c8.chunk.css"
+  },
+  {
+    "revision": "843b0fba9c92f4c1220b",
+    "url": "/static/css/1.a8ee2e74.chunk.css"
+  },
+  {
+    "revision": "f3d3eff4c680cdaec5c07ff19e3ae445",
+    "url": "/index.html"
+  }
+];
